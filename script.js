82999052a8d44f91ba241b82ab057d5eb07000d1
kevinlimaads <117680529+kevinlimaads@users.noreply.github.com>
2025-06-30 20:23:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const botao = document.querySelector("#botao");
-const mensagem = document.querySelector("#mensagem")
+const mensagem = document.querySelector("#mensagem");
 
 botao.addEventListener('click', function(){
     mensagem.textContent = "Você é o que estiver disposto a lutar para ser!"
